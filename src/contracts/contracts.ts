28857f198c4cecfb858640fe93a1a80d276effea
git.bc <wxznrl@gmail.com>
2025-10-29 14:08:01 -0300

    recaptcha

--- src/contracts/contracts.ts
+++ src/contracts/contracts.ts
@@ -65,3 +65,12 @@ export interface UI {
   sport: string;
   url: string;
 }
+
+export interface ReCaptchaResponse {
+  success: boolean;
+  score?: number;
+  action?: string;
+  challenge_ts?: string;
+  hostname?: string;
+  "error-codes"?: string[];
+}
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,22 @@
 import dotenv from "dotenv";
 import express from "express";
 import session from "express-session";
+import fetch from "node-fetch";
 import { gateway } from "./handlers/handler";
 import path from "path";
+import { ReCaptchaResponse } from "./contracts/contracts";
+import fs from "fs";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.API_PORT;
 
 declare module "express-session" {
   interface SessionData {
     user?: { token: string };
   }
 }
-
-dotenv.config();
-
-const app = express();
-const PORT = process.env.API_PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -23,44 +26,80 @@ app.use(
     secret: process.env.SECRET_KEY!,
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: true },
+    cookie: { secure: false },
   })
 );
 
-const isAuthenticated = (
-  req: express.Request,
-  res: express.Response,
-  next: express.NextFunction
-) => {
-  if (req.session.user) {
-    next();
-  } else {
-    res.redirect("/");
-  }
-};
-
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "login.html"));
+  const htmlPath = path.join(__dirname, "public", "login.html");
+  let html = fs.readFileSync(htmlPath, "utf-8");
+
+  const siteKey = process.env.RECAPTCHA_SITE_KEY!;
+  html = html.replace(/__RECAPTCHA_SITE_KEY__/g, siteKey);
+
+  res.send(html);
 });
-app.use(express.static(path.join(__dirname, "public")));
 
-app.post("/login", (req, res) => {
-  const { token, password } = req.body;
+app.post("/login", async (req, res) => {
+  const { token, password, "g-recaptcha-response": recaptchaToken } = req.body;
+  const secretKey = process.env.RECAPTCHA_SECRET_KEY!;
+  console.log(secretKey);
+  if (!recaptchaToken || !secretKey) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Token de segurança ausente." });
+  }
 
-  if (token === process.env.TOKEN && password === process.env.PASSWORD) {
-    req.session.user = { token };
-    res.redirect("/odds");
-  } else {
-    res.redirect("/?error=Credenciais inválidas");
+  try {
+    const verifyUrl = "https://www.google.com/recaptcha/api/siteverify";
+    const verifyResponse = await fetch(verifyUrl, {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: `secret=${process.env
+        .RECAPTCHA_SECRET_KEY!}&response=${recaptchaToken}&remoteip=${req.ip}`,
+    });
+    console.log(verifyResponse);
+
+    const verifyData = (await verifyResponse.json()) as ReCaptchaResponse;
+
+    if (
+      verifyData.success &&
+      verifyData.action === "login" &&
+      (verifyData.score ?? 0) >= 0.5
+    ) {
+      if (token === process.env.TOKEN && password === process.env.PASSWORD) {
+        req.session.user = { token };
+        return res.json({ success: true });
+      } else {
+        return res.json({ success: false, message: "Credenciais inválidas." });
+      }
+    } else {
+      console.log("reCAPTCHA falhou:", verifyData);
+      return res
+        .status(400)
+        .json({ success: false, message: "Verificação de segurança falhou." });
+    }
+  } catch (error) {
+    console.error("Erro na verificação reCAPTCHA:", error);
+    return res.status(500).json({ success: false, message: "Erro interno." });
   }
 });
 
-app.get("/odds", isAuthenticated, (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+app.get(
+  "/odds",
+  (req, res, next) => {
+    if (req.session.user) return next();
+    res.redirect("/");
+  },
+  (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+  }
+);
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.post("/api/gateway/:method", gateway.handleGateway);
 
 app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
